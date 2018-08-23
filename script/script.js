8c21dc8f07ac3c0a983da931659da4292bca76d3
Heeyoung <hyoon635@songareeit.com>
2018-08-23 10:59:35 +0900

    [메인] 좌측상단 버거버튼 눌렀을 때 사이드 네비게이션 바 사라지면서 그 영역까지 갖을 수 있도록 기능 추가

--- script/script.js
+++ script/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $(".burger-button").click(function() {
     $(".side-nav").toggleClass("hide");
+
+    if ($(".side-nav").hasClass("hide")) {
+      $("main").css("grid-column-start", "1");
+    } else {
+      $("main").css("grid-column-start", "2");
+    }
   });
 });
