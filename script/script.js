eaa3787110c2f288f6e4c0d2c869c19d178fa8a6
Heeyoung <hyoon635@songareeit.com>
2018-08-23 17:12:53 +0900

    [메인] 비디오 리스트 코드 정리 (for 문으로 태그 복제)

--- script/script.js
+++ script/script.js
@@ -8,4 +8,22 @@ $(document).ready(function() {
       $("main").css("grid-column-start", "2");
     }
   });
+
+  var Video = [
+    { img: "assets/img/video1.jpg" },
+    { img: "assets/img/video2.jpg" },
+    { img: "assets/img/video3.jpg" },
+    { img: "assets/img/video4.jpg" },
+    { img: "assets/img/video5.jpg" },
+    { img: "assets/img/video6.jpg" }
+  ];
+
+  var VideoList = "";
+  for (var i = 0; i < Video.length; i++) {
+    VideoList +=
+      '<a href="#" class="area"><div class="video-wrap"><img src="' +
+      Video[i].img +
+      '" alt="video" class="video-img"><span class="time">1:24:39</span><p>Wanderlust 🌲 - An Indie/Folk/Pop Playlist</p><span>alexrainbridMusic<i class="fas fa-check-circle"></i></span><span>조회수 16만회 ・ 3주전</span></div></a>';
+  }
+  $(".video-list").html(VideoList);
 });
